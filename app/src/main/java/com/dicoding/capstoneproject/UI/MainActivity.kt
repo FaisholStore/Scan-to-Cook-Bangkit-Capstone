07464a2616117ec6e891eee1ca18bb5fca59db15
faishol_store <faishol2561@gmail.com>
2023-12-13 14:06:05 +0700

    testing:

---
+++ app/src/main/java/com/dicoding/capstoneproject/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.capstoneproject.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.capstoneproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
